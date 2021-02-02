a0d4069b8115ba2923e1283863619bad2a4fdce9
ZhengZhenyu <zheng.zhenyu@foxmail.com>
2021-02-02 11:02:17 +0800

    Add script to count visit
    Add script to count visit

---
+++ assets/scripts/baidustat.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?5def612917161d75998e840a01d72a58";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
